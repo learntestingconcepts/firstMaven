d195bbc7be9bbecd9196f02d661c4ea07461f2e6
learntestingconcepts <35674645+learntestingconcepts@users.noreply.github.com>
2018-01-23 07:45:55 +0530

    Add files via upload

---
+++ src/main/java/com/learn/testing/firstMaven/firstTestNg.java
@@ -0,0 +1,12 @@
+package com.learn.testing.firstMaven;
+
+import org.testng.annotations.Test;
+
+public class firstTestNg {
+
+	@Test
+	public void firstTestNgProgram()
+	{
+		System.out.println("hello first tesNg Program");
+	}
+}
